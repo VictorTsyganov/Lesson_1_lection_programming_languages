15eab8c1eaa38a657a6bfdaa48fe8de348316d61
Victor Tsyganov <tsyganov1989@gmail.com>
2022-10-12 16:50:10 +0300

    add Different_greeting

---
+++ Different_greeting/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите имя пользовател: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "Маша")
+{
+    Console.Write("Ура, это же Маша!");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
